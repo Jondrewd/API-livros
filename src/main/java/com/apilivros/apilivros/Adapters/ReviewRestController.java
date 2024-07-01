65d9863c22c2f985d10ffe403073a81c1aa0baed
Jonas <jonas220703@gmail.com>
2024-07-01 19:37:48 -0300

    Create ReviewRestController.java

---
+++ src/main/java/com/apilivros/apilivros/Adapters/ReviewRestController.java
@@ -0,0 +1,10 @@
+package com.apilivros.apilivros.Adapters;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/reviews")
+public class ReviewRestController {
+
+}

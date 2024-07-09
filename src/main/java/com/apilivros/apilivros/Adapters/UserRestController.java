d22f6015b32a7d991451afb30ff4c957f05a26b9
Jonas <jonas220703@gmail.com>
2024-07-09 20:04:27 -0300

    Update UserRestController.java
    p

--- src/main/java/com/apilivros/apilivros/Adapters/UserRestController.java
+++ src/main/java/com/apilivros/apilivros/Adapters/UserRestController.java
@@ -1,10 +1,43 @@
 package com.apilivros.apilivros.Adapters;
 
+import java.net.URI;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.apilivros.apilivros.Domain.User;
+import com.apilivros.apilivros.Services.UserService;
 
 @RestController
 @RequestMapping("/users")
 public class UserRestController {
 
+    @Autowired
+    private UserService service;
+
+    @GetMapping
+    public ResponseEntity<List<User>> findAll(){
+        List<User> list = service.findAll();
+        return ResponseEntity.ok().body(list);
+    }
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Integer id){
+        User user = service.findById(id);
+        return ResponseEntity.ok().body(user);
+    }
+
+    @PostMapping
+    public ResponseEntity<User> insert(RequestBody User obj){
+        obj = service.insert(id);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUriString();
+        return ResponseEntity.created(uri).body(obj);
+    }
 }
